27c4c33553e36be06611d54e382a97d5e3864c30
hesy01 <hesy01@mingyuanyun.com>
2022-11-04 16:57:18 +0800

    feat: 添加数据

--- pages/list/EditModal.tsx
+++ pages/list/EditModal.tsx
@@ -1,8 +1,88 @@
-import { Modal } from "antd";
-import React from "react";
-const EditModal =() => {
-  return (
-    <Modal></Modal>
-  )
+import React, { forwardRef, ReactNode, useImperativeHandle, useState } from "react"
+import { Button, Drawer, Form, Input, Radio, Space, DatePicker, Select } from "antd"
+import { useForm } from "antd/lib/form/Form"
+
+
+import { DataType } from "./data"
+type RefHandle = {
+  toggleModal: (visible?: boolean, data?: DataType) => void
 }
-export default EditModal
+
+const CreateModal = forwardRef<RefHandle>((ref) => {
+  const [visible, setVisible] = useState<boolean>(false)
+  const [plan_type, setPlan] = useState<DataType>()
+  const [modalForm] = useForm()
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleModal: async (vis: boolean, data:DataType) => {
+        if(data) {
+          setPlan(data)
+        } 
+        setVisible(vis)
+      },
+    }
+  })
+
+  // 取消创建【重新销毁组件】
+  const onRest = () => {
+    setPlan(undefined)
+    modalForm.resetFields()
+    setVisible(false)
+  }
+
+  // 创建
+  const onSubmit = async () => {
+    const { operation_name, operation_type, service_unit, chart_list, date, project_id, org_code, icon_url } =
+      modalForm.getFieldsValue()
+    if (await modalForm.validateFields()) {
+      console.log("nihao ")
+    }
+    const chartList = chart_list.map((i: { value: string; label: ReactNode }) => {
+      return {
+        chart_name: i.label,
+        bizcode: i.value,
+      }
+    })
+
+    setPlan(undefined)
+    modalForm.resetFields()
+    setVisible(false)
+  }
+
+  return (
+    <Drawer
+      width={640}
+      visible={visible}
+      destroyOnClose
+      title="设置"
+      closable={false}
+      // extra={<CloseOutlined onClick={() => onRest()} />}
+      footerStyle={{
+        display: "flex",
+        justifyContent: "flex-end",
+      }}
+      footer={
+        <Space>
+          <Button onClick={() => onRest()}> 取消</Button>
+          <Button type="primary" onClick={() => onSubmit()}>
+            确定
+          </Button>
+        </Space>
+      }
+    >
+      <Form form={modalForm}>
+        <Form.Item label="产品名称" required name="name" >
+          <Input placeholder="请输入运营名称"/>
+        </Form.Item>
+        <Form.Item label="产品型号" required name="size">
+          <Input placeholder="请输入产品型号"/>
+        </Form.Item>
+        <Form.Item label="产品说明" required name="remark">
+          <Input placeholder="请输入产品说明"/>
+        </Form.Item>
+      </Form>
+    </Drawer>
+  )
+})
+export default CreateModal
