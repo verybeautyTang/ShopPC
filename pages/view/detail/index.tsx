b087ed4817745ad21e4269e060ea7665f46ee29b
verybeautytang <shiyu.he@happyelements.com>
2022-11-10 19:45:16 +0800

    fix: add pages

---
+++ pages/view/detail/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+const Detail = () => {
+    return (
+        <div>我是Detail</div>
+    )
+}
+export default Detail
---
+++ pages/view/project/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+const Project = () => {
+    return (
+        <div>我是Project</div>
+    )
+}
+export default Project
